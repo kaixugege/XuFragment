4be0a6384bf25dcb87222f78b9476c3f92fa3c9e
kaixugege <604312074@qq.com>
2019-05-07 14:38:02 +0800

    Initial commit

---
+++ xu_fragment/src/main/java/com/xu/gege/fragment/MyClass.java
@@ -0,0 +1,16 @@
+package com.xu.gege.fragment;
+
+/**
+ * @Author: KaixuGege
+ * Time:           2019/5/7
+ * ProjectName:    XuFragment
+ * ClassName:
+ * Info:
+ */
+public class MyClass {
+
+    public void test() {
+        System.out.println("It's OK.");
+    }
+
+}
